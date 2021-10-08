86f94ed12951cdefab9505bc9f542a9326b3d662
sustxh <sustinianvanxh@gmail.com>
2021-10-08 17:19:08 +0300

    initialize Wall objects with WallGrid

--- QuoridorGame.Model/Entities/WallsGrid.cs
+++ QuoridorGame.Model/Entities/WallsGrid.cs
@@ -9,6 +9,13 @@ namespace QuoridorGame.Model.Entities
         public WallsGrid()
         {
            Grid = new Wall[GridSize, GridSize];
+           for (int i = 0; i < GridSize; i++) 
+           {
+                for (int j = 0; j < GridSize; j++)
+                {
+                    Grid[i, j] = new Wall();
+                }
+            }
         }
         public void WallIsPlaceable(WallType walltype, int x, int y)
         {
