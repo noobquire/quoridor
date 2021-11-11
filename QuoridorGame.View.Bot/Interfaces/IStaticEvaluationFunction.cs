836b58930b9b2d52b30e643ece998bc000ab1443
sustxh <sustinianvanxh@gmail.com>
2021-11-11 22:55:57 +0200

    Randeom SEV added as a baseline

---
+++ QuoridorGame.View.Bot/Interfaces/IStaticEvaluationFunction.cs
@@ -0,0 +1,10 @@
+﻿using Game = QuoridorGame.Model.Entities.QuoridorGame;
+using QuoridorGame.Model.Entities;
+
+namespace QuoridorGame.View.Bot.Interfaces
+{
+    interface IStaticEvaluationFunction
+    {
+        public float Eval(Game game, Player evalplayer);
+    }
+}
---
+++ QuoridorGame.View.Bot/Logic/RandomSEV.cs
@@ -0,0 +1,15 @@
+﻿using QuoridorGame.View.Bot.Interfaces;
+using QuoridorGame.Model.Entities;
+using Game = QuoridorGame.Model.Entities.QuoridorGame;
+using System; 
+
+namespace QuoridorGame.View.Bot.Logic
+{
+    class RandomSEV : IStaticEvaluationFunction
+    {
+        public float Eval(Game game, Player evalplayer) {
+            Random rand = new Random();
+            return (float)rand.NextDouble();
+        }
+    }
+}
